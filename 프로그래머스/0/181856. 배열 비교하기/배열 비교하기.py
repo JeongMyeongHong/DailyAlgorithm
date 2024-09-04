61295c6fd561da8a435381ff1fe62bcb76ebfdef
홍정명 <97998301+JeongMyeongHong@users.noreply.github.com>
2024-09-04 22:39:45 +0900

    [level 0] Title: 배열 비교하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.py
@@ -0,0 +1,16 @@
+def solution(arr1, arr2):
+    len_arr1 = len(arr1)
+    len_arr2 = len(arr2)
+    if len_arr1 == len_arr2:
+        sum_arr1 = sum(arr1)
+        sum_arr2 = sum(arr2)
+        if sum_arr1 == sum_arr2:
+            return 0
+        elif sum_arr1 > sum_arr2:
+            return 1
+        else: 
+            return -1
+    elif len_arr1 > len_arr2:
+        return 1
+    else:
+        return -1
